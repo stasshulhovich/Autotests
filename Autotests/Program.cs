4094530eda9165f3e3618704fbf541b6d906bee4
stasshulhovich <stas.shulhovich@gmail.com>
2020-11-23 14:11:03 +0300

    Azure autotests

---
+++ Autotests/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Autotests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.demoqa.com";
+            driver.Url = "https://www.google.com";
+            driver.Url = "https://www.yandex.ru";
+            driver.Quit();
+        }
+    }
+}
